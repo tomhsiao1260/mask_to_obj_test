a4f752a012d45e44e3b2d0f8b1d1f09c590383c0
Hsiao Yao <tomtogo1260@gmail.com>
2024-10-13 03:50:53 +0800

    complete the normals part via open3d

--- mask_to_obj.py
+++ mask_to_obj.py
@@ -3,6 +3,7 @@ import open3d
 import tifffile
 import argparse
 import numpy as np
+import open3d as o3d
 
 from scipy.spatial import Delaunay
 from skimage.morphology import skeletonize
@@ -10,6 +11,7 @@ from slice_to_point import process_slice_to_point
 
 def save_obj(filename, data):
     vertices = data.get('vertices', np.array([]))
+    normals  = data.get('normals' , np.array([]))
     uvs      = data.get('uvs'     , np.array([]))
     faces    = data.get('faces'   , np.array([]))
 
@@ -19,9 +21,14 @@ def save_obj(filename, data):
 
         for i in range(len(vertices)):
             vertex = vertices[i]
+            normal = normals[i]
 
             f.write('v ')
             f.write(f"{' '.join(str(round(x, 2)) for x in vertex)}")
+            f.write('\n')
+
+            f.write('vn ')
+            f.write(f"{' '.join(str(round(x, 6)) for x in normal)}")
             f.write('\n')
 
         for uv in uvs:
@@ -30,6 +37,15 @@ def save_obj(filename, data):
         for face in faces:
             indices = ' '.join(['/'.join(map(str, vertex)) for vertex in face])
             f.write(f"f {indices}\n")
+
+# update normals
+def compute_normals(mesh):
+  if not mesh.triangle_normals:
+    mesh.compute_vertex_normals()
+    mesh.triangle_normals = o3d.utility.Vector3dVector([])
+  else:
+    mesh.compute_vertex_normals()
+    mesh.compute_triangle_normals()
 
 # python mask_to_obj.py
 if __name__ == '__main__':
@@ -107,7 +123,14 @@ if __name__ == '__main__':
     mask = (edge_0 < max_d) & (edge_1 < max_d) & (edge_2 < max_d)
     data['faces'] = data['faces'][mask] 
 
-    # [1, 3, 2] -> [[1, 1, 1], [3, 3, 3], [2, 2, 2]]
+    # use open3d to compute normals
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(data['vertices'])
+    mesh.triangles = o3d.utility.Vector3iVector(data['faces'])
+    compute_normals(mesh)
+    data['normals'] = np.asarray(mesh.vertex_normals)
+
+    # switch faces format [1, 3, 2] -> [[1, 1, 1], [3, 3, 3], [2, 2, 2]]
     data['faces'] += 1
     data['faces'] = np.repeat(data['faces'], 3).reshape(-1, 3, 3)
 
